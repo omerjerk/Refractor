51668d53a83b998051b35514144586ea719df1b0
Umair Khan <omerjerk@gmail.com>
2017-04-02 20:18:10 +0530

    finish setup code
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- util/general.py
+++ util/general.py
@@ -1,7 +1,21 @@
 import os
 from os.path import expanduser
+import platform
 
-def default_workspace_location():
+def get_apktool_loc():
+    pform = platform.system().lower()
+    if pform == "darwin":
+        dir_name = "darwin"
+    elif pform == "windows":
+        dir_name = "win"
+    elif pform == "linux":
+        dir_name = "linux"
+    else:
+        print "Unsupported Operating System"
+        return
+    return "lib/" + dir_name + "/apktool"
+
+def default_workspace_loc():
     home = expanduser("~")
     workspace = home + '/RefractorWorkspace'
     if not os.path.exists(workspace):
